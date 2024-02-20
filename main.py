ec09952d70f52d04e571c588a065ed90d07ed259
Prateek Mohan <65453254+prateek-mohan@users.noreply.github.com>
2024-02-20 13:22:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,27 @@ def home(request: Request):
 @app.get("/")
 def home():
     return "Hello"
+@app.post("/uploads/")
+async def upload_image(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data))
+    
+    # Save the image to a temporary file
+    temp_file_path = 'temp_img.jpg'
+    image.save(temp_file_path)
+    
+    # Call the predict function using subprocess.Popen
+    results=subprocess.Popen(["python", "predictWithOCR.py", "model='best.pt'", f"source={temp_file_path}"], stdout=subprocess.PIPE)
+    output, error = results.communicate()
+    
+    # Delete the temporary file
+    os.remove(temp_file_path)
+    i= get_image_data_from_blob()
+    
+   # file_path = "runs/detect/train/temp_img.jpg"  # replace with your actual file path
+    # Return the output
+    return Response(content=i, media_type='image/jpeg')
+    
+    
    
     
