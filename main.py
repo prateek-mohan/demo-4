cbbbcc410192af5a7a76820850c0cf927f6c9d44
Prateek Mohan <65453254+prateek-mohan@users.noreply.github.com>
2024-02-20 12:46:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
+templates=Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home(request: Request):
