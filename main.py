a56ff5e82766696d069624bbea706b0d5af2d7f6
Prateek Mohan <65453254+prateek-mohan@users.noreply.github.com>
2024-02-20 13:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = FastAPI()
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 templates=Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/abc")
 def home(request: Request):
-    return templates.TemplateResponse("upload.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+def home():
+    return "Hello"
+   
+    
